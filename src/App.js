064395bbebb04f37973cacb20142a11d65588abb
Kai Curry <kai@sundaysenergy.com>
2015-09-24 20:34:28 -0500

    broken

--- src/App.js
+++ src/App.js
@@ -1,49 +1,17 @@
-import React, { Component } from 'react';
-import { NICE, SUPER_NICE } from './colors';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import Counter from './Counter';
+import * as Actions from './redux/actions';
 
-import SampleForm from './SampleForm'
-
-class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      counter: 0
-    };
-    this.interval = setInterval(() => this.tick(), 2000);
-  }
-
-  tick() {
-    this.setState({
-      counter: this.state.counter + this.props.increment
-    });
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-  render() {
-    const {color, increment} = this.props
-    const {counter} = this.state
-
-    return (
-      <h1 style={{ color: color }}>
-        Counter ({increment}): {counter}
-      </h1>
-    );
-  }
+// This is where we define computed fields (reselect module) or make other changes.
+function mapStateToProps(state) {
+  return {
+    counter: state.counter
+  };
 }
 
-export class App extends Component {
-
-  render() {
-    return (
-      <div className="wrapper">
-        <header>
-          <Counter increment={2} color={SUPER_NICE} />
-        </header>
-        <SampleForm />
-      </div>
-    );
-  }
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(Actions, dispatch);
 }
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
